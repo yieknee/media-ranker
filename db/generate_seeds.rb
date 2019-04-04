f79e0b2017ebba5cccf585f05342b4d1e631f7eb
CheezItMan <chris@adadevelopersacademy.org>
2019-04-04 11:37:10 -0700

    run rufo on the document

--- db/generate_seeds.rb
+++ db/generate_seeds.rb
@@ -1,6 +1,6 @@
-require 'faker'
-require 'date'
-require 'csv'
+require "faker"
+require "date"
+require "csv"
 
 # we already provide a filled out media_seeds.csv file, but feel free to
 # run this script in order to replace it and generate a new one
@@ -10,14 +10,13 @@ require 'csv'
 # $ rails db:reset
 # doesn't currently check for if titles are unique against each other
 
-CSV.open('db/media_seeds.csv', "w", :write_headers=> true,
-  :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
-
+CSV.open("db/media_seeds.csv", "w", :write_headers => true,
+                                    :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
   25.times do
     category = %w(album book).sample
     title = Faker::Coffee.blend_name
     creator = Faker::Name.name
-    publication_year = rand(Date.today.year-100..Date.today.year)
+    publication_year = rand(Date.today.year - 100..Date.today.year)
     description = Faker::Lorem.sentence
 
     csv << [category, title, creator, publication_year, description]
