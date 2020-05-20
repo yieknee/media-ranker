8a5e3c885a88c84a9a39caac1ef67409da8a8c9f
Yieni <yaynee8d@gmail.com>
2020-05-20 16:03:29 -0700

    create new work functionality with form, flash, and validation

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,29 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+   @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+   
+    if @work.save 
+      flash[:success] = "Successfully created #{@work.category} #{@work.id} "
+      redirect_to work_path(@work.id)
+      return
+    else 
+      flash.now[:error] = "A problem occurred: Could not create #{@work.category}"
+      render :new, status: :bad_request
+      return
+    end
+    
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description )
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,4 @@
 class Work < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+
 end
