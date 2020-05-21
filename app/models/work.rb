715812fcf259dc03f8eac10c14756d61cb00781f
Yieni <yaynee8d@gmail.com>
2020-05-20 17:27:12 -0700

    refactor top 10 and spotlight

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,4 +6,17 @@ class Work < ApplicationRecord
     return works.sample
   end
 
+  def self.top_ten(category: category)
+    works = Work.where(category: category)
+    top_ten = []
+    if works.count < 10
+      top_ten = works
+    else
+      10.times do
+        top_ten << works.sample
+      end
+    end
+    return top_ten
+  end
+
 end
